830cb61bbdd2b3d7cb6c9028ba1cd55c43ea20cf
UtkarshaAnand <utkarshaanand123@gmail.com>
2020-08-27 21:08:12 +0530

    Updates

---
+++ precache-manifest.62f3edc6baa98eaddfb301aef0ddaa8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fedf067678a98d0164aa5054532d6e6",
+    "url": "/Real-Madrid/index.html"
+  },
+  {
+    "revision": "88333c983a32a748c53f",
+    "url": "/Real-Madrid/static/css/main.b35b089b.chunk.css"
+  },
+  {
+    "revision": "51f2849041b4c65167c4",
+    "url": "/Real-Madrid/static/js/2.1440c2b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Real-Madrid/static/js/2.1440c2b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88333c983a32a748c53f",
+    "url": "/Real-Madrid/static/js/main.224db073.chunk.js"
+  },
+  {
+    "revision": "7fac6816f1a21e7f0df4",
+    "url": "/Real-Madrid/static/js/runtime-main.98ab2408.js"
+  }
+]);
